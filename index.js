28fb1de55f909592731506bca51bfa68aeb63191
IntsarK <162660098+IntsarK@users.noreply.github.com>
2025-08-13 19:38:00 -0500

    Add documentation

--- index.js
+++ index.js
@@ -19,24 +19,35 @@ let topMovies = [
 ];
 
 app.use(morgan("common"));
-
 app.use(express.static("public"));
 
-// GET requests
+// 1) Welcome message
+// Method: GET
+// URL: /
+// Query params: none
+// Body: none
+// Returns: text message
 app.get("/", (req, res) => {
   res.send("Welcome to myFlix API!");
 });
 
+// 2) Return all movies
+// Method: GET
+// URL: /movies
+// Query params: none
+// Body: none
+// Returns: JSON array of movies [{ title, author }]
 app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
 
+// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
 
-// listen for requests
+// Start server
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
 });
